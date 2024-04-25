9047f740a886052dbc3eb336440498a76902dcb2
Wizqqr <baban1979molotov@gmail.com>
2024-04-25 15:26:43 +0600

    Исправил парсер

--- house_parser/car_kg.py
+++ house_parser/car_kg.py
@@ -9,24 +9,27 @@ class HouseCrawler:
     MAIN_URL = "https://www.house.kg/snyat"
     BASE_URL = "https://www.house.kg"
 
-    def get_page(self):
-        response = httpx.get(self.MAIN_URL)
-        self.page = response.text
-
-    def get_title(self):
-        html = Selector(self.page)
+    async def get_page(self):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(self.MAIN_URL)
+            self.page_content = response.text
+            return response
+    async def get_title(self):
+        html = Selector(self.page_content)
         title = html.css("title::text").get()
         return title
 
-    def get_car_links(self):
-        html = Selector(self.page)
-        links = html.css(".list-item a::attr(href)").getall()
-        full_links = list(map(lambda x: self.BASE_URL + x, links))
-        return full_links[:3]
+    async def get_car_links(self):
+        html = Selector(self.page_content)
+        link = html.css(".list-item a::attr(href)").getall()
+        full_link = list(map(lambda x: self.BASE_URL + x, link))
+        print(full_link)
+        return full_link[:-1]
 
-@dp.message(Command("links"))
+@car_router.message(Command("links"))
 async def links(message: types.Message):
     crawler = HouseCrawler()
-    crawler.get_page()
-    crawler.get_car_links()
-    await message.answer(crawler.get_car_links())
+    await crawler.get_page()
+    title = await crawler.get_title()
+    link = await crawler.get_car_links()
+    await message.answer(f"Имя: {title}, ссылка {link}")
