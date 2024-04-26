dc603f220662fc19530d201ebd61a94853af73c1
Wizqqr <baban1979molotov@gmail.com>
2024-04-26 17:56:11 +0600

    поменял dz6

--- house_parser/car_kg.py
+++ house_parser/car_kg.py
@@ -1,3 +1,42 @@
+# import httpx
+# from parsel import Selector
+# from bot import dp, types
+# from aiogram import Router
+# from aiogram.filters import Command
+#
+# car_router = Router()
+#
+#
+# class HouseCrawler:
+#     MAIN_URL = "https://www.house.kg/snyat"
+#     BASE_URL = "https://www.house.kg"
+#
+#
+#     async def get_page(self):
+#             async with httpx.AsyncClient() as client:
+#                 response = await client.get(self.MAIN_URL)
+#                 self.page_content = response.text
+#                 return response
+#     async def get_title(self):
+#             html = Selector(self.page_content)
+#             title = html.css("title::text").get()
+#             return title
+#
+#     async def get_car_links(self):
+#         html = Selector(self.page_content)
+#         links = html.css('.property-list .property-item a::attr(href)').getall()
+#         full_links = [self.BASE_URL + link for link in links]
+#         print(full_links)
+#         return full_links
+#
+#     @car_router.message(Command("links"))
+#     async def links(message: types.Message):
+#         crawler = HouseCrawler()
+#         await crawler.get_page()
+#         title = await crawler.get_title()
+#         link = await crawler.get_car_links()
+#         await message.answer(f"Имя: {title}, ссылка {link}")
+#
 import httpx
 from parsel import Selector
 from bot import dp, types
@@ -5,6 +44,8 @@ from aiogram import Router
 from aiogram.filters import Command
 
 car_router = Router()
+
+
 class HouseCrawler:
     MAIN_URL = "https://www.house.kg/snyat"
     BASE_URL = "https://www.house.kg"
@@ -14,6 +55,7 @@ class HouseCrawler:
             response = await client.get(self.MAIN_URL)
             self.page_content = response.text
             return response
+
     async def get_title(self):
         html = Selector(self.page_content)
         title = html.css("title::text").get()
@@ -21,15 +63,16 @@ class HouseCrawler:
 
     async def get_car_links(self):
         html = Selector(self.page_content)
-        link = html.css(".list-item a::attr(href)").getall()
-        full_link = list(map(lambda x: self.BASE_URL + x, link))
-        print(full_link)
-        return full_link[:-1]
+        links = html.css('.property-list .property-item a::attr(href)').getall()
+        full_links = [self.BASE_URL + link for link in links]
+        print(full_links)
+        return full_links
 
-@car_router.message(Command("links"))
-async def links(message: types.Message):
-    crawler = HouseCrawler()
-    await crawler.get_page()
-    title = await crawler.get_title()
-    link = await crawler.get_car_links()
-    await message.answer(f"Имя: {title}, ссылка {link}")
+    @car_router.message(Command("links"))
+    async def links(message: types.Message):
+        crawler = HouseCrawler()
+        await crawler.get_page()
+        title = await crawler.get_title()
+        link = await crawler.get_car_links()
+        await message.answer(f"Имя: {title}, ссылка {link}")
+
